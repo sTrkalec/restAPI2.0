63543e92ee04196095d105d9660c926e9a02ebf4
Gabriel Miranda <vytorgabriel123v@gmail.com>
2021-01-25 11:29:46 -0300

    Erro 404 criado quando nao se encontra uma rota no app.js

--- app.js
+++ app.js
@@ -5,16 +5,25 @@ const morgan = require("morgan");
 const routeProducts = require("./routes/products");
 const routePedidos = require("./routes/pedidos");
 
-app.use(morgan("dev"))
+app.use(morgan("dev"));
 
-app.use("/products", routeProducts);
+app.use("/produtos", routeProducts);
 app.use("/pedidos", routePedidos);
 
 //Quando nao encontra a rota, entra aqu:
 app.use((req, res, next)=>{
     const erro = new Error("Nao encontrado");
-    erro.status(404);
+    erro.status = 404;
     next(erro);
+});
+
+app.use((error, req, res, next) => {
+   res.status (error.status || 500);
+   return res.send({
+        erro: {
+            mensagem: error.message
+        }
+   });
 });
 
 
