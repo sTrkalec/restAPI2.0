02e5316667c147009ac3b00a6e934dd89db022d2
Gabriel Miranda <vytorgabriel123v@gmail.com>
2021-02-19 14:07:29 -0300

    Add MYSQL CONNECTION and two routes GET/POST

--- routes/products.js
+++ routes/products.js
@@ -1,24 +1,45 @@
 const express = require("express");
 const router = express.Router();
+const mysql = require ("../mysql").pool;
 
 //RETORNA TODOS OS PRODUTOS
 router.get("/", (req, res, next) => {
-    res.status(200).send({
-        mensagem: "Retorna todos os produtos"
+    mysql.getConnection((error, conn) => {
+        if (error) { return res.status(500).send({error : error}) };
+        conn.query(
+            "SELECT * FROM produtos",
+            (error, resultado, fields) =>{
+                if (error) { return res.status(500).send({error : error}) };
+
+                return res.status(200).send({response: resultado})
+            }
+        )
+
+
     })
 });
+
+
+
 //INSERE  UM PRODUTO
 router.post("/", (req, res, next) => {
+    mysql.getConnection((error, conn) =>{
+        if (error) { return res.status(500).send({error : error}) };
+        conn.query(
+            "INSERT INTO produtos (nome, preco) VALUES (?,?)",
+            [req.body.nome, req.body.preco],
+            (error, resultado, field) => {
+                conn.release();
+                if (error) { return res.status(500).send({error : error}) };
 
-    const produto ={
-        nome: req.body.nome,
-        preco: req.body.preco
-    }
-
-    res.status(201).send({
-        mensagem: "Insere um produto",
-        produtoCriado: produto
+                res.status(201).send({
+                    mensagem: "produtos inserido com sucesso",
+                    id_produto: resultado.insertiD
+                });
+            }
+        )
     });
+
 });
 //RETORNA OS DADOS DE UM PRODUTO
 router.get("/:id_produto", (req, res, next) => {
